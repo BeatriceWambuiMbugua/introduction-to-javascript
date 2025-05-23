7ce7e226e1f3081206e125fb39372c91b00cc2e4
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2025-05-23 10:03:06 +0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// Data Types
+
+// Expression
+
+// Three Types of Expressions
+
+// Constant Expression
+
+5
+"Hello World"
+
+"Good Morning, How are you?"
+
+let goodMorningGreeting = "Good Morning, How are you?"
+
+const theDaysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const theYearOfBirth = 1990;
+ 
+goodMorningGreeting = "Good Morning, how did you sleep?"
+
+
+console.log(goodMorningGreeting)
+console.log(theDaysOfTheWeek)
+console.log(theYearOfBirth)
